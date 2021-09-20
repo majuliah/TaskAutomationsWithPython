b022673ff2b21eedd47a6df067d07726bf9c5755
Majulia <majulia_carvalho@yahoo.com.br>
2021-09-20 01:34:05 -0300

    1/3 finalizado

--- twitterBot.py
+++ twitterBot.py
@@ -33,32 +33,7 @@ mundi = (" O total de casos no Mundo: \n" + \
           " Óbitos: " + deaths+"\n" + \
           " Recuperações: " + recovered + "\n\n")
 
-          
-
-
-
-print(brasil)
-print(mundi)
-
-url = "https://hooks.zapier.com/hooks/catch/10934489/b6srkfc"
-data = {'value1': brasil, 'value2': mundi}
-headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-r = requests.post(url, data=json.dumps(data), headers=headers)
-print(r.text)
-
-print("Sleeping for 6 hours")
-time.sleep(7200)
-
-#api clima-tempo
-
-# tolkien = 'e28c15b24acee4755e78e314c3ee096b'
-# tipoConsulta = 1
-
-# if tipoConsulta == 1:
-#     ##não pedir o nome da cidade, enviar por default
-
-## "http://apiadvisor.climatempo.com.br/api/v1/locale/city?name=São Paulo&state=SP&token=your-app-token"
-
+#Tempo
 token = 'e28c15b24acee4755e78e314c3ee096b'
 city = '6037'
 
@@ -67,8 +42,35 @@ response = requests.request('GET', url)
 retorno = json.loads(response.text)
 # print(retorno)
 
-for key in retorno:
-  print(key + ': ' + str(retorno[key]))
+# for key in retorno:
+#  print(key + ': ' + str(retorno[key]))
 
-for key in retorno['data']:
-  print(key + ': ' + str(retorno['data'][key]))
+cidadeNome = retorno['name']
+cidadeEstado = retorno['state']
+temperatura = retorno['data']['temperature']
+condicao = retorno['data']['condition']
+sensacao = retorno['data']['sensation']
+velocidadeVento = retorno['data']['wind_velocity']
+dia = retorno['data']['date']
+
+
+clima = ("O clima em " + cidadeNome + "-" + cidadeEstado + " : " + dia + "\n" + \
+          "Temperatura = " + temperatura + "ºC \n" + \
+          "Sensação térmica = " + sensacao + "ºC \n" + \
+          "Ventos de = " + velocidadeVento + "km/h \n" + \
+          "O dia está = " + condicao + " .\n\n")
+
+
+# for key in retorno['data']:
+#   print(key + ': ' + str(retorno['data'][key]))
+  
+url = "https://hooks.zapier.com/hooks/catch/10934489/b6srkfc"
+data = {'value1': brasil, 'value2': clima, 'value3': mundi}
+headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+r = requests.post(url, data=json.dumps(data), headers=headers)
+print(r.text)
+
+print("Sleeping for 2 hours")
+time.sleep(7200)
+
+##abrir o twitter e dar f5 na pagina
